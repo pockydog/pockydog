d0db7d2d00db2a1c3ad96537297d2987ca8b06f7
vickychen <vickyvhen@fillygaming888.com>
2022-02-15 09:47:42 +0800

    3

--- test.1.py
+++ test.1.py
@@ -5,8 +5,8 @@ import sys
 # print(sys.argv)
 # print(sys.path)
 # os.system("df -h")
-print(os.path.isabs())
-
+# print(os.path.isabs())
+print(os.listdir())
 
 # print(os.getcwd())
 # print(os.listdir())
