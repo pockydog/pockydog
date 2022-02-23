0938ebac1d7c4f99dab62961641d1ba082d814d6
vickychen <vickyvhen@fillygaming888.com>
2022-02-23 16:41:35 +0800

    record

--- test_01.py
+++ test_01.py
@@ -26,25 +26,22 @@ class Test:
                 for type in types:
                     name = type.find('a').get('href')
                     title_name.append(f'{cls._domain}{name}')
+            print(title_name)
             return title_name
 
-        @classmethod
-        def get_url(cls):
-            title_name = cls.get_type_list()
-            item_list = list()
-            for title in title_name:
-                response = requests.get(url=f'{title}')
-                soup = BeautifulSoup(response.text, 'html.parser')
-                pages = soup.find('a', {'class': 'grid__itemTop--xogBBJ'})
-                print(pages)
+
+        # @classmethod
+        # def get_url(cls):
+        #     title_name = cls.get_type_list()
+        #     item_list = list()
+            # for title in title_name:
+            #     response = requests.get(url=f'{title}')
+        #         soup = BeautifulSoup(response.text, 'html.parser')
+        #         pages = soup.find('a', {'class': 'grid__itemTop--xogBBJ'})
+        #         print(pages)
 
 
-            #     for page in pages:
-            #         item = page.get('href')
-            #         item_list.append(item)
-            # print(item_list)
-            # return item_list
-        # #
+
         # # @classmethod
         # # def get_po(cls):
         # #     item_list = cls.get_url()
@@ -54,7 +51,7 @@ class Test:
 
 if __name__ == '__main__':
 #     # Test.get_url()
-#     Test.get_type_list()
+    Test.get_type_list()
     Test.get_url()
 #     # Test.get_po()
 
