afba1330cf7b728645cbade38e1cbd72c84eb7eb
vickychen <vickyvhen@fillygaming888.com>
2022-03-11 18:12:14 +0800

    ddd

--- test wheel.py
+++ test wheel.py
@@ -5,3 +5,12 @@
 # import re
 # a = https://steam.oxxostudio.tw/category/python/basic/inheritance.html
 print(a)
+# https://hackmd.io/1XuDG3lJTZGuLL4Zao8fmg\
+a = https://yuhsiangfu.gitbooks.io/ice101-basic-python-programming-and-data-analysis/content/class-6-han-shu/64-can-zhao-ji-chuan-ru-can-zhao.html
+https://www.delftstack.com/zh-tw/howto/python/python-change-dictionary-value/
+https://www.delftstack.com/zh-tw/tutorial/python-modules-tutorial/python-regular-expression/#%E4%BD%BF%E7%94%A8-sub-%E5%87%BD%E5%BC%8F%E4%BE%86%E5%88%AA%E9%99%A4%E5%AD%97%E4%B8%B2%E4%B8%AD%E7%9A%84%E6%89%80%E6%9C%89%E6%95%B8%E5%AD%97
+https://www.jb51.net/shouce/jquery/regexp.html
+https://www.learncodewithmike.com/2020/01/python-module-and-package.html
+
+print(a)
+
